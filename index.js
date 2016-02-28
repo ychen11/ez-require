df07c1e75dc1abd375578debbe003019f58a91f4
ychen11 <chenyiwei1987@gmail.com>
2016-02-28 23:32:50 +0800

    checkin some files

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 var Root = {
-  path: null
+  globalDir: null,
+  localDirs: []
 };
+
+var _= require('underscore');
 
 
 /**
  * Set the root directory.
  * @param {String} the root directory path
  */
-exports.setRoot = function (rootPath) {
-  Root.path = rootPath;
+exports.setGlobalDir = function (path) {
+  Root.globalDir = path;
 };
 
-exports.ezrequire = function (dir) {
-  if (!Root.path) {
+/**
+ * ezrequire function
+ * @param {String} path the partial path from global dir to current file.
+ */
+exports.globalRequire = function (path) {
+  if (!Root.globalDir) {
     throw new Error('Root directory not set');
   }
 
   return require(Root.path + dir);
 };
 
+
+/**
+ * Set the local path, if the local path
+ * existed, then reset it, if not, add it.
+ * @param {String} path the local path
+ */
+exports.setLocalRoot = function (path) {
+
+  if (_.contains(Root.localDirs, path)) {
+    return;
+  } else {
+    Root.localDirs.push(path);
+    return;
+  }
+}
